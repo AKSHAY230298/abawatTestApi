6a4d2f07566a1db2a3e45bc3c7183d9f39850f2b
AKSHAY230298 <akshu.pokalekar98@gmail.com>
2025-03-12 13:11:04 +0530

    update

--- controllers/matDetailsController.js
+++ controllers/matDetailsController.js
@@ -1,6 +1,26 @@
-async function handleMatDetails(){
+const MatDetails = require("../models/matDetails");
+
+async function handleMatDetails(req,res){
     try {
-        req.body
+  const {type,subject,team,department} = req.body;
+  const matData = MatDetails.find()
+
+        if(!matData){
+          return  res
+           .status(400)
+            . json({
+                message:"Data is empty",
+                success:false
+            })
+
+            
+        }
+        return res.status(200)
+        .json({
+            message:"ok",
+            success:true,
+            matData : matData
+        })
     } catch (error) {
         console.log(error);
         
